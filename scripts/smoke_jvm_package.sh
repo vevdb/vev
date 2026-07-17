0a413ea0af4c949f3c4622b743920c11daddbd1d
Andreas Flakstad <git@andreasflakstad.no>
2026-07-17 15:22:37 +0200

    run jvm package smokes from source files

--- scripts/smoke_jvm_package.sh
+++ scripts/smoke_jvm_package.sh
@@ -34,28 +34,32 @@ cat > "$TMP_DIR/deps-java.edn" <<EOF
                             "--enable-native-access=ALL-UNNAMED"]}}}
 EOF
 
-(
-  cd "$TMP_DIR"
-  clojure -Sdeps "$(cat deps-java.edn)" -M:run -e "(import '[dev.vevdb.vev Vev Vev\$TxReportListener])
+cat > "$TMP_DIR/java-smoke.clj" <<'EOF'
+(import '[dev.vevdb.vev Vev Vev$TxReportListener])
 (with-open [vev (Vev/load)
             conn (.createConn vev)]
   (let [seen (atom 0)]
-    (with-open [listener (.listen conn \"java-listener\"
-                                  (reify Vev\$TxReportListener
+    (with-open [listener (.listen conn "java-listener"
+                                  (reify Vev$TxReportListener
                                     (accept [_ report]
-                                      (when (.contains (str report) \":user/listener\")
+                                      (when (.contains (str report) ":user/listener")
                                         (swap! seen inc)))))]
-      (.transact conn \"[[:db/add 1 :user/listener \\\"heard\\\"]]\")
+      (.transact conn "[[:db/add 1 :user/listener \"heard\"]]")
       (assert (= 1 @seen)))
-    (.transact conn \"[[:db/add 1 :user/listener \\\"after-close\\\"]]\")
+    (.transact conn "[[:db/add 1 :user/listener \"after-close\"]]")
     (assert (= 1 @seen)))
-  (.transact conn \"[{:db/id 1 :user/name \\\"Ada\\\"}]\")
+  (.transact conn "[{:db/id 1 :user/name \"Ada\"}]")
   (with-open [db (.db conn)]
     (let [rows (.queryRows vev (java.util.Map/of
-                                 \"query\" \"[:find ?name :where [?e :user/name ?name]]\"
-                                 \"args\" (java.util.List/of db)))]
-      (assert (= [[\"Ada\"]] (vec (map vec rows))))))
-  (println :vev-java-package-ok))"
+                                 "query" "[:find ?name :where [?e :user/name ?name]]"
+                                 "args" (java.util.List/of db)))]
+      (assert (= [["Ada"]] (vec (map vec rows))))))
+  (println :vev-java-package-ok))
+EOF
+
+(
+  cd "$TMP_DIR"
+  clojure -Sdeps "$(cat deps-java.edn)" -M:run java-smoke.clj
 )
 
 cat > "$TMP_DIR/deps-clj.edn" <<EOF
@@ -65,27 +69,31 @@ cat > "$TMP_DIR/deps-clj.edn" <<EOF
                             "--enable-native-access=ALL-UNNAMED"]}}}
 EOF
 
-(
-  cd "$TMP_DIR"
-  clojure -Sdeps "$(cat deps-clj.edn)" -M:run -e "(require '[vev.core :as d])
+cat > "$TMP_DIR/clojure-smoke.clj" <<'EOF'
+(require '[vev.core :as d])
 (let [conn (d/create-conn)]
   (let [seen (atom [])]
     (with-open [listener (d/listen conn :clj-listener #(swap! seen conj (:tx-data %)))]
-      (d/transact conn [[:db/add 1 :user/listener \"heard\"]])
+      (d/transact conn [[:db/add 1 :user/listener "heard"]])
       (assert (= 1 (count @seen))))
-    (d/transact conn [[:db/add 1 :user/listener \"after-close\"]])
+    (d/transact conn [[:db/add 1 :user/listener "after-close"]])
     (assert (= 1 (count @seen))))
-  (d/transact conn [{:db/id 1 :user/name \"Ada\"}])
-  (assert (= #{[\"Ada\"]}
+  (d/transact conn [{:db/id 1 :user/name "Ada"}])
+  (assert (= #{["Ada"]}
              (d/q '[:find ?name :where [?e :user/name ?name]]
                   (d/db conn))))
   (with-open [fns (d/tx-fns conn {:user/set-name
                                   (fn [db e name]
                                     [[:db/add e :user/name name]])})]
     (d/transact conn [[:db/add 100 :db/ident :user/set-name]])
-    (d/transact conn [[:user/set-name 2 \"Grace\"]] fns)
-    (assert (= #{[\"Grace\"]}
+    (d/transact conn [[:user/set-name 2 "Grace"]] fns)
+    (assert (= #{["Grace"]}
                (d/q '[:find ?name :where [2 :user/name ?name]]
                     (d/db conn)))))
-  (println :vev-jvm-package-ok))"
+  (println :vev-jvm-package-ok))
+EOF
+
+(
+  cd "$TMP_DIR"
+  clojure -Sdeps "$(cat deps-clj.edn)" -M:run clojure-smoke.clj
 )
